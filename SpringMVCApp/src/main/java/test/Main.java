caad259644730c0a2d0aaf03f4cdd58ff147cc89
Ngô Đình Nguyên <nguyennd56coltech@gmail.com>
2014-04-09 02:43:34 +0700

    tt

--- SpringMVCApp/src/main/java/test/Main.java
+++ SpringMVCApp/src/main/java/test/Main.java
@@ -1,7 +1,15 @@
 package test;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
+
+import javax.xml.stream.XMLInputFactory;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.FileInputStream;
 
 /**
  * Created by Nguyen D. Ngo on 4/9/14.
@@ -9,7 +17,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         System.out.println("test start");
-        ApplicationContext context = new ClassPathXmlApplicationContext("mvc-dispatcher-servlet.xml");
+        try {
+            InputStream is = new FileInputStream("mvc-dispatcher-servlet.xml");
+            XmlBeanFactory xbf = new XmlBeanFactory((Resource) is);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //ApplicationContext context = new ClassPathXmlApplicationContext("mvc-dispatcher-servlet.xml");
         System.out.println("test finish");
     }
 }
